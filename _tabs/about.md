67245fd0fa69ee2521a608c4b75ad64e91e8e85f
RoanYiu <146944246+GitRoan@users.noreply.github.com>
2024-08-02 00:37:38 +0800

    修改关于标签跟简历标签。

--- _tabs/about.md
+++ _tabs/about.md
@@ -1,8 +1,6 @@
 ---
-# the default layout is 'page'
+layout: about
+title: about
 icon: fas fa-info-circle
 order: 5
 ---
-
-> Add Markdown syntax content to file `_tabs/about.md`{: .filepath } and it will show up on this page.
-> {: .prompt-tip }
--- 运行本地服务器.bat
+++ 运行本地服务器.bat
@@ -1,2 +1,16 @@
 @echo off
-bundle exec jekyll s
+chcp 65001 >nul
+
+rem Set RUBYOPT environment variable to suppress deprecation warnings
+rem RUBYOPT is used to pass additional command line options to the Ruby interpreter
+rem Setting RUBYOPT to suppress deprecation warnings【Suppress: YJIT】
+set RUBYOPT=-W:no-deprecated
+
+rem Run Jekyll local server
+rem The command 'bundle exec jekyll serve' starts the Jekyll local server
+rem The server will run at http://127.0.0.1:4000/
+rem bundle exec jekyll clean
+bundle exec jekyll serve
+
+ chcp 936 >nul
+ pause
